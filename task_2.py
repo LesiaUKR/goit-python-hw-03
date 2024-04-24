75c6df0eb2f52586f93830505727bcc2ae9d07df
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-04-24 20:07:39 +0300

    feat: solve task 2

--- task_2.py
+++ task_2.py
@@ -14,5 +14,5 @@ def get_numbers_ticket(min, max, quantity):
         return
 
 
-lottery_numbers = get_numbers_ticket(0, 49, 6)
-print("Ваші лотерейні числа:", lottery_numbers)
+lottery_numbers = get_numbers_ticket(1, 49, 10)
+print("Your lottery digits:", lottery_numbers)
